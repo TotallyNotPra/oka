2e4c8ef17d0f243458ab7f74676c7f83b10276f5
Pra <pranavucool@gmail.com>
2025-10-13 23:10:23 +0530

    a

--- d.py
+++ d.py
@@ -2,9 +2,10 @@
 import numpy as np
 import sklearn as d
 a = int(input("yeah:"))
+vocals = {"a":0, "b":22,"c":2}
 if a == 2:
-    x = np.array([1,2,36,7])
-    y = np.array([2,3,4,25])
+    x = np.array([vocals["a"],vocals["b"],vocals['b']])
+    y = np.array([2,3,4])
     z = np.prod(x)
     print('f(x) + (y)', x + y)
     print('f(x)*f(y)',z)
