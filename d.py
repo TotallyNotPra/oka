c2d87a9718350dd0d20688642581e699d8fa0249
Pra <pranavucool@gmail.com>
2025-10-13 22:28:52 +0530

    yeah

--- d.py
+++ d.py
@@ -2,9 +2,10 @@
 import numpy as np
 import sklearn as d
 a = int(input("yeah:"))
-
-def roblox(x,y):
-    gaming = np.array()
-bob = roblox(x=a,y=a)
-print(bob)
-
+if a == 2:
+    x = np.array([1,2,36,7])
+    y = np.array([2,3,4,25])
+    z = np.prod(x)
+    print('f(x) + (y)', x + y)
+    print('f(x)*f(y)',z)
+    
